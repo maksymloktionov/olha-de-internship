3e66ea809996272a575b9045d5431f75aee0a2bd
olha <polischuk280603@gmail.com>
2024-09-18 17:24:46 +0300

    Add more descriptive aliases

--- sql_puzzle.sql
+++ sql_puzzle.sql
@@ -1,13 +1,13 @@
 -- Write your PostgreSQL query statement below
-WITH CTE_emp AS  
+WITH CTE_employee_salary_rank AS  
 ( 
 SELECT d.name AS Department, e.name AS Employee,e.salary AS Salary ,
-DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC )AS rn
+DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC )AS salary_rank
 FROM Employee AS e
 INNER JOIN Department AS d
     ON e.departmentId = d.id
 )
 
 SELECT Department, Employee, Salary 
-FROM CTE_emp
-WHERE rn BETWEEN 1 AND 3
+FROM CTE_employee_salary_rank
+WHERE salary_rank <= 3
