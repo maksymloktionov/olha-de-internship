5a65ff34b00accda5144b09462c30b2ff740643a
olha <polishuk280603@gmail.com>
2024-10-08 18:33:23 +0300

    Changes to python script

--- _python/scriptmodified.py
+++ _python/scriptmodified.py
@@ -14,7 +14,7 @@ class Wiki_ParseWar:
     #this is an instance method
     def __init__(self,url):
         self.url = url
-        logging.info(f'DATABASE_URL: {os.getenv("DATABASE_URL")}')
+        logging.info('Database connetion was establish successfully')
     
     @staticmethod
     def get_Page(url):
@@ -66,10 +66,10 @@ class Wiki_ParseWar:
        
        logging.info('Finished cleaning data')
 
-    def csv_Save(self,csv_path):
-        logging.info(f"Saving data to CSV: {csv_path}")
-        self.df.to_csv(csv_path,index=False)  
-        logging.info('Saved')
+    #def csv_Save(self,csv_path):
+     #   logging.info(f"Saving data to CSV: {csv_path}")
+      #  self.df.to_csv(csv_path,index=False)  
+       # logging.info('Saved')
 
     def db_Con(self, table_name):
         logging.info(f'Saving data to database: {table_name}')
@@ -77,24 +77,27 @@ class Wiki_ParseWar:
         if not database_url:
             logging.error('DATABASE_URL is not set.')
             return 
-        engine = create_engine(database_url)
-        self.df.to_sql(
-            name=table_name,
-            con=engine,
-            if_exists="append",
-            index=False
-        )
-        logging.info('Saved')
+        try:
+            engine = create_engine(database_url)
+            self.df.to_sql(
+                name=table_name,
+                con=engine,
+                if_exists="append",
+                index=False
+            )
+            logging.info('Saved')
+        except Exception as e:
+            logging.error(f'Error saving to database:{e}')
 
    
-    def run(self,table_index,csv_path,table_name):
+    def run(self,table_index,table_name):
         soup = self.get_Page(self.url)
         if soup:
             self.parse_tables(soup,table_index)
             self.pd_Clean()
-            self.csv_Save(csv_path)
+           # self.csv_Save(csv_path)
             self.db_Con(table_name)
 if __name__ == "__main__":
     url = 'https://en.wikipedia.org/wiki/List_of_wars_involving_Russia#Russian_Federation_(1991%E2%80%93present'
     scraper = Wiki_ParseWar(url)   
-    scraper.run(table_index=8,csv_path='/home/olha/olha-de-internship/file.csv',table_name='warcrimes')
+    scraper.run(table_index=8,table_name='warcrimes')
