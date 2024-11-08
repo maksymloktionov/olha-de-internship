920940c4ba12c5e2983bda878882231b09d0c6c6
olha <polishuk280603@gmail.com>
2024-11-08 19:12:47 +0200

    Create incremental models for weather

---
+++ personal_project/_dbt_weather/models/marts/staging/stg_kyiv_data.sql
@@ -0,0 +1,29 @@
+{{
+    config (
+        materialized = 'incremental',
+        unique_key = "date"
+    )
+}}
+
+SELECT 
+    1 as city_id,
+    city_name,
+    date,
+    tavg as avg_temperature,
+    tmin as min_temperature,
+    tmax as max_temperature,
+    prcp as total_precipitation,
+    snow as snow_depth,
+    wdir as wind_direction,
+    wspd as avg_wind_speed,
+    wpgt as wind_peak_gust,
+    pres as sea_level_air_pressure,
+    tsun as total_sunshine_duration
+
+FROM {{source('landing','stg_kyiv_data')}}
+
+
+{% if is_incremental() %}
+    where "date"::date >= (select coalesce (max("date"::date),'1970-01-01'::date) from {{this}})
+
+{% endif %}
