bd45197fd60172d06ea34854512be98fbd7128d7
olha <polishuk280603@gmail.com>
2024-11-13 19:51:52 +0200

    Transformations for weather analysis

--- personal_project/_dbt_weather/models/marts/facts/fact_weather.sql
+++ personal_project/_dbt_weather/models/marts/facts/fact_weather.sql
@@ -1,40 +1,50 @@
-with dim_city as (
-    select city_id 
-    from {{ ref('dim_city') }}
-),
-dim_country as (
-    select country_id
-    from {{ ref('dim_country') }}
-),
-dim_time as ( 
-    select time_id 
-    from {{ ref('dim_time') }}
-),
-aggregated_data as (
-    select 
-        city_id,
-        country_id,
-        time_id,
-        avg_temp,
-        min_temp,
-        max_temp,
-        total_precipitation,
-        avg_wind_speed,
-        avg_wind_direction
-    from {{ ref('int_cities_join') }}
+with daily_weather as (
+
+    select distinct
+        {{ dbt_utils.generate_surrogate_key(['c.city_id', 'd.date_key']) }} as fact_weather_key,
+        c.city_id,
+        cd.country_id,
+        d.date_key,
+        c.date, 
+        c.avg_temperature,
+        c.min_temperature,
+        c.max_temperature,
+        c.total_precipitation,
+        c.snow_depth,
+        c.wind_direction,
+        c.avg_wind_speed,
+        c.wind_peak_gust,
+        c.sea_level_air_pressure,
+        c.total_sunshine_duration,
+        
+        avg(c.avg_temperature) over (partition by  year(c.date)) as yearly_avg_temp,
+        min(c.min_temperature) over (partition by  year(c.date)) as yearly_min_temp,
+        max(c.max_temperature) over (partition by  year(c.date)) as yearly_max_temp,
+        sum(c.total_precipitation) over (partition by year(c.date)) as yearly_precipitation,
+        sum(c.snow_depth) over (partition by year(c.date)) as yearly_snow_depth,
+        avg(c.avg_wind_speed) over (partition by year(c.date)) as yearly_avg_wind_speed,
+        case 
+            when c.wind_peak_gust >= 80 then 'Extreme'
+            when c.wind_peak_gust >= 50 and c.wind_peak_gust < 80 then 'Strong'
+            else 'Moderate'     
+            end as wind_gust_category,
+        case 
+            when c.sea_level_air_pressure < 1000 then 'Low'
+            when c.sea_level_air_pressure between 1000 and 1020 then 'Normal'
+            else 'High'
+            end as sea_air_pressure,
+
+        sum(c.total_sunshine_duration) over (partition by month(c.date)) as monthly_sunshine_hours,
+        case 
+            when c.total_sunshine_duration < 3 then 'Low'
+            when c.total_sunshine_duration between 3 and 8 then 'Moderate'
+            else 'High'
+        end as sunshine_duration
+
+    from {{ ref('combined_cities_countries') }} c
+    left join {{ ref('dim_date') }} d on c.date = d.date_day
+    left join {{ ref('dim_cities_countries') }} cd on c.city_id = cd.city_id
+
 )
 
-select distinct
-    dc.city_id,
-    country.country_id,
-    dt.time_id,
-    ad.avg_temp,
-    ad.min_temp,
-    ad.max_temp,
-    ad.total_precipitation,
-    ad.avg_wind_speed,
-    ad.avg_wind_direction
-from aggregated_data ad
-join dim_city dc on ad.city_id = dc.city_id
-join dim_country country on ad.country_id = country.country_id
-join dim_time dt on ad.time_id = dt.time_id
+select * from daily_weather
--- personal_project/_dbt_weather/models/marts/staging/stg_cities.sql
+++ personal_project/_dbt_weather/models/marts/staging/stg_cities.sql
@@ -1,13 +1,14 @@
 {{
     config (
         materialized = 'incremental',
-        unique_key = "date"
+        unique_key = "city_id"
     )
 }}
 
+
 SELECT 
-    2 as city_id,
-    city_name,
+    city_id,
+    city,
     date,
     tavg as avg_temperature,
     tmin as min_temperature,
@@ -20,10 +21,11 @@ SELECT
     pres as sea_level_air_pressure,
     tsun as total_sunshine_duration
 
-FROM {{source('landing','stg_munich_data')}}
+FROM {{source('landing','stg_cities')}}
 
 
 {% if is_incremental() %}
-    where date::date >= (select coalesce (max(date::date),'1970-01-01'::date) from {{this}})
+
+    where date::date >= (select max(date::date) from {{this}})
 
 {% endif %}
