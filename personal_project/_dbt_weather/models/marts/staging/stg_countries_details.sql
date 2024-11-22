6e1dfa415bb3f7869e2b603df104574401cbf928
olha <polishuk280603@gmail.com>
2024-11-22 10:08:41 +0200

    Incremental models strategy

--- personal_project/_dbt_weather/models/marts/staging/stg_countries_details.sql
+++ personal_project/_dbt_weather/models/marts/staging/stg_countries_details.sql
@@ -6,3 +6,9 @@
 }}
 
 select * from  {{source('landing','stg_countries_details')}}
+
+{% if is_incremental() %}
+
+    where date::date >= (select max(date::date) from {{this}})
+
+{% endif %}
--- personal_project/dbt-dag/dbt_weather/models/marts/staging/stg_countries_details.sql
+++ personal_project/dbt-dag/dbt_weather/models/marts/staging/stg_countries_details.sql
@@ -1,8 +1,15 @@
 {{
     config (
         materialized = 'incremental',
-        unique_key = "country_id"
+        unique_key = "country_id",
+        
     )
 }}
 
 select * from  {{source('landing','stg_countries_details')}}
+
+{% if is_incremental() %}
+
+    where date::date >= dateadd(day, -3 , current_date())
+
+{% endif %}
