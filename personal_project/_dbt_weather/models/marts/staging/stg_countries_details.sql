a1d15bdf7ed56cd4f6df21f527f7c1e395fee8cd
olha <polishuk280603@gmail.com>
2024-11-24 17:54:54 +0200

    Add loaded_at

--- personal_project/_dbt_weather/models/marts/staging/stg_countries_details.sql
+++ personal_project/_dbt_weather/models/marts/staging/stg_countries_details.sql
@@ -1,14 +1,28 @@
-{{
-    config (
-        materialized = 'incremental',
-        unique_key = "country_id"
-    )
-}}
+{{ config(
+    materialized = 'incremental',
+    unique_key = "country_id"
+) }}
 
-select * from  {{source('landing','stg_countries_details')}}
+SELECT 
+    country_id,
+    country,
+    city,
+    region,
+    continent,
+    latitude,
+    longitude,
+    agricultural_land,
+    forest_area,
+    land_area,
+    rural_land,
+    urban_land,
+    co2_emissions,
+    methane_emissions,
+    loaded_at 
+FROM {{ source('landing', 'stg_countries_details') }}
 
 {% if is_incremental() %}
-
-    where date::date >= (select max(date::date) from {{this}})
+    
+where loaded_at >= (select coalesce(max(loaded_at),'1900-01-01') from {{ this}})
 
 {% endif %}
